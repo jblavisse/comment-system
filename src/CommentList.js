6d5287a2935df942d36596c94d432a14f884d870
jblavisse <contact@jlavisse.com>
2020-06-22 10:01:53 +0200

    Remove explanation props

--- src/CommentList.js
+++ src/CommentList.js
@@ -2,22 +2,11 @@ import React, { Component } from 'react';
 
 class CommentList extends Component {
     render() { 
-
-        //this.props.comments
-        /* [  
-        { name: "JB", message: "Youhou la famille!"},
-        { name: "Kirikou", message: "Je ne suis pas grand mais je suis vaillant"}
-        ] */ 
-
         let commentsList = this.props.comments.map(comment => {
             return <li>{comment.name}: {comment.message}</li>
          })
 
-        /* [
-        <li>JB: Youhou la famille!</li>,
-        <li>Kirikou: Je ne suis pas grand mais je suis vaillant</li>
-       ] */  
-
+        let isVisible = true;
 
         return (
             <ul className="comment-list">
